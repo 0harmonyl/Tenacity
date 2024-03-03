9886a0166ec4aa891228a49688978e35057d9edb
0harmonyl <142665949+0harmonyl@users.noreply.github.com>
2024-03-03 13:10:41 +0100

    Initial Commit

---
+++ Tenacity/Tenacity/Loader.cs
@@ -0,0 +1,13 @@
+﻿using BepInEx;
+using Utilla;
+
+namespace Tenacity
+{
+    [ModdedGamemode]
+    [BepInDependency("org.legoandmars.gorillatag.utilla", "1.6.11")]
+    [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+    public class Loader : BaseUnityPlugin 
+    {
+        
+    }
+}
---
+++ Tenacity/Tenacity/PluginInfo.cs
@@ -0,0 +1,9 @@
+namespace Tenacity
+{
+    internal class PluginInfo
+    {
+        public const string Name = "Tenacity";
+        public const string GUID = "org.opensource.tenacity";
+        public const string Version = "1.0";
+    }
+}
